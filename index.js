0bd40f2c99011444daf6f68c894b3cb479e160a1
Diana C Peterson <dpeter10@gmail.com>
2022-09-12 12:45:44 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name = "Aki") {
+     return `Hi, my name is ${name}.`
+}
+function logTwoValues(value1, value2) {
+    return `The two values are ${value1} and ${value2}.`
+  }
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function sayHelloTo(firstName = "User") {
+    return `Hello,${firstName}!`
+  }
+  
+function introductionWithLanguageOptional(name, language = "JavaScript") 
+{ 
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+}
